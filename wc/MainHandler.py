a87157c5b01c0b8b0759098857d6316d94045541
Michael Pace <mpace1027@gmail.com>
2012-06-26 16:53:31 -0500

    updated request handler

---
+++ wc/MainHandler.py
@@ -0,0 +1,26 @@
+import os
+from google.appengine.ext import webapp
+from google.appengine.ext.webapp import template
+from google.appengine.ext.webapp.util import run_wsgi_app
+
+class MainPage(webapp.RequestHandler):
+    def get(self):
+        page = self.request.get('page')
+        template_values = {
+            'page': page,
+        }
+
+        #path = os.path.join(os.path.dirname(__file__), page + ".html")
+        path = os.path.join(os.path.dirname(__file__), "index.html")
+        self.response.out.write(template.render(path, template_values))
+
+
+application = webapp.WSGIApplication(
+                                     [('/', MainPage)],
+                                     debug=True)
+
+def main():
+    run_wsgi_app(application)
+
+if __name__ == "__main__":
+    main()
