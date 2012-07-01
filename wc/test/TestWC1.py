94bb832b69438d5724ff3815bddde2a7c847bdaa
joe <joe@vbm13.(none)>
2012-07-01 18:41:53 -0500

    starting on unit tests

---
+++ wc/test/TestWC1.py
@@ -0,0 +1,33 @@
+import unittest
+from MainHandler import ExportPage, ImportPage, exportLinks, grabLinks
+
+crisis_list = []
+person_list = []
+organization_list = []
+link_list = []
+
+class ExportTests(unittest.TestCase):
+	def test_tests(self):
+		self.assertEqual(True, True)
+		
+	def test_exportperson(self):
+		
+	def test_exportorg(self):
+		
+	def test_exportcrisis(self):
+
+	def test_exportlinks(self):
+		
+		
+class ImportTests(unittest.TestCase):
+	def test_tests(self):
+		self.assertEqual(True, True)
+	
+	def test_importperson(self):
+		
+	def test_importorg(self):
+		
+	def test_importcrisis(self):
+		
+	def test_grablinks(self):
+		
