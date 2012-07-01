bc288550a9c20dc63f2479bd91d8ee56ad06bf69
joe <joe@vbm13.(none)>
2012-07-01 16:15:47 -0500

    moved export to ExportHandler

--- wc/MainHandler.py
+++ wc/MainHandler.py
@@ -21,57 +21,9 @@ class MainPage(webapp.RequestHandler):
         self.response.out.write(template.render(path, template_values))
 
 
-class ExportPage(webapp.RequestHandler):
-    def get(self):
-        crises = db.GqlQuery("SELECT * FROM Crisis")
-              
-        xml_string = '<?xml version="1.0" encoding="UTF-8"?><worldCrises xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"xsi:noNamespaceSchemaLocation="wc.xsd">'
-        
-        for crisis in crises:
-            xml_string += "<crisis id=\"" + crisis.crisisid + "\">"
-            xml_string += "<name>" + crisis.name + "</name>"
-            xml_string += "<info>"
-            xml_string += "<history>" + crisis.info_history + "</history>"
-            xml_string += "<help>" + crisis.info_help + "</help>"
-            xml_string += "<resources>" + crisis.info_resources + "</resources>"
-            xml_string += "<type>" + crisis.info_type + "</type>"
-            xml_string += "<time>"
-            xml_string += "<time>" + str(crisis.date_time) + "</time>"
-            xml_string += "<day>" + str(crisis.date_day) + "</day>"
-            xml_string += "<month>" + str(crisis.date_month) + "</month>"
-            xml_string += "<year>" + str(crisis.date_year) + "</year>"
-            xml_string += "<misc>" + (crisis.date_misc or "") + "</misc>"
-            xml_string += "</time>"
-            xml_string += "<loc>"
-            xml_string += "<city>" + (crisis.location_city or "") + "</city>"
-            xml_string += "<region>" + (crisis.location_region or "") + "</region>"
-            xml_string += "<country>" + (crisis.location_country or "") + "</country>"
-            xml_string += "</loc>"
-            xml_string += "<impact>"
-            xml_string += "<human>"
-            xml_string += "<deaths>" + str(crisis.impact_human_deaths) + "</deaths>"
-            xml_string += "<displaced>" + str(crisis.impact_human_displaced) + "</displaced>"
-            xml_string += "<injured>" + str(crisis.impact_human_injured) + "</injured>"
-            xml_string += "<missing>" + str(crisis.impact_human_missing) + "</missing>"
-            xml_string += "<misc>" + str(crisis.impact_human_misc or "") + "</misc>"
-            xml_string += "</human>"
-            xml_string += "<economic>"
-            xml_string += "<amount>" + str(crisis.impact_economic_amount) + "</amount>"
-            xml_string += "<currency>" + str(crisis.impact_economic_currency or "") + "</currency>"
-            xml_string += "<misc>" + str(crisis.impact_economic_misc or "") + "</misc>"
-            xml_string += "</economic>"
-            xml_string += "</impact>"
-            xml_string += "</info>"
-            xml_string += "</crisis>"
-        xml_string += "</worldCrises>"
-        
-        #print xml_string
-        self.response.headers['Content-Type'] = "text/xml; charset=utf-8"
-        self.response.out.write(xml_string)
 
-application = webapp.WSGIApplication([('/', MainPage),
-                                      ('/export', ExportPage)],
-                                     debug=True)
+application = webapp.WSGIApplication([('/', MainPage)],debug=True)
+
 def main():
     run_wsgi_app(application)
             
