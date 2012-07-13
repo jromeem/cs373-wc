74f590d1d97971b320352c26edf7058ddb102e38
joe <joe@joe-K53U.(none)>
2012-07-12 19:48:10 -0500

    adds documentation

--- wc/MainHandler.py
+++ wc/MainHandler.py
@@ -1,16 +1,27 @@
-import cgi, os
+import cgi, os, sys
 import cgitb; cgitb.enable()
+"""
+sys.path.append('/home/joe/Downloads/google_appengine/google')
+sys.path.append('/home/joe/Downloads/google_appengine/google/appengine')
+sys.path.append('/home/joe/Downloads/google_appengine/google/appengine/ext')
+sys.path.append('/home/joe/Downloads/google_appengine/google/appengine/ext/webapp')
+from google import appengine
+from google.appengine import ext
+"""
 from google.appengine.ext import webapp
 from google.appengine.ext.webapp import template
 template.register_template_library('django.contrib.humanize.templatetags.humanize')
 from google.appengine.ext.webapp.util import run_wsgi_app
 from google.appengine.ext import db
 from DataModels import Crisis, Organization, Person
+
 import django.contrib.humanize.templatetags.humanize
+
 
 class MainPage(webapp.RequestHandler):
     def get(self):
         headerNote = " - Import Antigravity"
+        #: test documentation for images
         images = db.GqlQuery("SELECT * FROM Link WHERE link_type='primaryImage' ORDER BY link_url")
         
         template_values={'page_name': 'World Crises','team_name': 'IMPORT ANTIGRAVITY','team_members': ['Joe Peacock', 'Andy Hsu','Harrison He','Jerome Martinez','Michael Pace','Justin Salazar',], 'images' : images, 'headerNote': headerNote}
