4ab1ceb3f520a72282d6eeee0b8d7209ad5a09dd
Michael Pace <mpace1027@gmail.com>
2012-07-25 20:08:25 -0500

    search results work

--- wc/Search.py
+++ wc/Search.py
@@ -26,40 +26,41 @@ class SearchResults(webapp.RequestHandler):
         datamodels = [crises.run(), orgs.run(), people.run()]
 
         index = 0
-        for kind in datamodels[index]:
-            entity_dict = kind.__dict__
-            # take out things you don't want to search in here
-            entity_dict.pop('_entity')
-            
-            for key, value in entity_dict.items():
-                # only parse normalized strings and unicode strings
-                if type(value) == type(u''):
-                    # convert to unicode
-                    target_string = unicodedata.normalize('NFKD', value).encode('ascii','ignore')
-                    target_string = target_string
-                else:
-                    target_string = repr(value)[:-1]
-                matched = regex_obj.match(target_string)
-                
-                if matched != None:
-                    snippet = '...' + matched.group(1) + '<b>' + matched.group(2) + '</b>' + matched.group(3) + '...'
-                    elemid = unicodedata.normalize('NFKD', entity_dict['_elemid']).encode('ascii','ignore')
-                    title = unicodedata.normalize('NFKD', entity_dict['_name']).encode('ascii','ignore')
-
-                    link_string = '<a href="/'
-                    if index == 0:
-                        link_string += 'crises/'
-                    elif index == 1:
-                        link_string += 'organizations/'
+        for kind in datamodels:
+            for entity in kind:
+                entity_dict = entity.__dict__
+                # take out things you don't want to search in here
+                entity_dict.pop('_entity')
+                for key, value in entity_dict.items():
+                    # only parse normalized strings and unicode strings
+                    if type(value) == type(u''):
+                        # convert to unicode
+                        target_string = unicodedata.normalize('NFKD', value).encode('ascii','ignore')
+                        target_string = target_string
                     else:
-                        link_string += 'people/'
-                    link_string += elemid + '">' + title + '</a>'
+                        target_string = repr(value)[:-1]
+                    matched = regex_obj.match(target_string)
                     
-                    snippet = re.sub(r'\.\.\.u.', r'...', snippet)
-                    
-                    result_string = [link_string, '<p>' + snippet + '</p>']
-                    search_results[str(i)] = result_string 
-                    i += 1
+                    if matched != None:
+                        snippet = '...' + matched.group(1) + '<b>' + matched.group(2) + '</b>' + matched.group(3) + '...'
+                        elemid = unicodedata.normalize('NFKD', entity_dict['_elemid']).encode('ascii','ignore')
+                        title = unicodedata.normalize('NFKD', entity_dict['_name']).encode('ascii','ignore')
+    
+                        link_string = '<a href="/'
+                        if index == 0:
+                            link_string += 'crises/'
+                        elif index == 1:
+                            link_string += 'organizations/'
+                        else:
+                            link_string += 'people/'
+                        link_string += elemid + '">' + title + '</a>'
+                        
+                        snippet = re.sub(r'\.\.\.\[?u.', r'...', snippet)
+                        snippet = re.sub(r'\'\.\.\.', r'...', snippet)
+                        
+                        result_string = [link_string, '<p>' + snippet + '</p>']
+                        search_results[str(i)] = result_string 
+                        i += 1
             index += 1
 
         self.response.out.write(json.dumps(search_results))
